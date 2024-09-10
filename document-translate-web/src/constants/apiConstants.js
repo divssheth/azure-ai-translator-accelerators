e3f00e6232cad050cdead7b14460b78fde0d6265
Divye Sheth <divyesheth@microsoft.com>
2024-09-10 10:56:56 +0100

    added translation service at the end of url

--- document-translate-web/src/constants/apiConstants.js
+++ document-translate-web/src/constants/apiConstants.js
@@ -1,5 +1,5 @@
 export const API_KEY = "70ff0feceded4b05aecc7af7c8f988ae";//process.env.REACT_APP_API_KEY;
-export const BASE_URL = "https://translator-apim-4365.azure-api.net";//process.env.REACT_APP_BASE_URL;
+export const BASE_URL = "https://translator-apim-4365.azure-api.net/translation-service";//process.env.REACT_APP_BASE_URL;
 
 export const GET_LOGS_API = (date) => `${BASE_URL}/get_logs_by_date?date=${date}`;
 export const GET_ALL_LOGS = `${BASE_URL}/get_all_logs`;
